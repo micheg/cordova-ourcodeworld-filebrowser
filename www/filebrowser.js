27ff69735fe399cd728e1f43f709fbf651b07f59
micheg <michelangelog@gmail.com>
2023-02-28 19:03:20 +0100

    fixes

--- www/filebrowser.js
+++ www/filebrowser.js
@@ -2,26 +2,32 @@
 
 module.exports = {
     filePicker: {
-        single: function (config) {
-            var successCallback = config.success || function(){};
-            var errorCallback = config.error || function(){};
+        single: function (config)
+        {
+            var successCallback = config.success || function () { };
+            var errorCallback = config.error || function () { };
             var startupPath = config.startupPath || "default";
 
-            cordova.exec(function(files){
-                if(typeof(successCallback) == "function"){
+            cordova.exec(function (files)
+            {
+                if (typeof (successCallback) == "function")
+                {
                     successCallback(JSON.parse(files));
                 }
             }, errorCallback, "Filebrowser", "showPicker", [{
                 "start_directory": startupPath
             }]);
         },
-        multi: function (config) {
-            var successCallback = config.success || function(){};
-            var errorCallback = config.error || function(){};
+        multi: function (config)
+        {
+            var successCallback = config.success || function () { };
+            var errorCallback = config.error || function () { };
             var startupPath = config.startupPath || "default";
 
-            cordova.exec(function(files){
-                if(typeof(successCallback) == "function"){
+            cordova.exec(function (files)
+            {
+                if (typeof (successCallback) == "function")
+                {
                     successCallback(JSON.parse(files));
                 }
             }, errorCallback, "Filebrowser", "showMultiFilepicker", [{
@@ -30,26 +36,32 @@ module.exports = {
         }
     },
     folderPicker: {
-        single: function (config) {
-            var successCallback = config.success || function(){};
-            var errorCallback = config.error || function(){};
+        single: function (config)
+        {
+            var successCallback = config.success || function () { };
+            var errorCallback = config.error || function () { };
             var startupPath = config.startupPath || "default";
 
-            cordova.exec(function(files){
-                if(typeof(successCallback) == "function"){
+            cordova.exec(function (files)
+            {
+                if (typeof (successCallback) == "function")
+                {
                     successCallback(JSON.parse(files));
                 }
             }, errorCallback, "Filebrowser", "showFolderpicker", [{
                 "start_directory": startupPath
             }]);
         },
-        multi: function (config) {
-            var successCallback = config.success || function(){};
-            var errorCallback = config.error || function(){};
+        multi: function (config)
+        {
+            var successCallback = config.success || function () { };
+            var errorCallback = config.error || function () { };
             var startupPath = config.startupPath || "default";
 
-            cordova.exec(function(files){
-                if(typeof(successCallback) == "function"){
+            cordova.exec(function (files)
+            {
+                if (typeof (successCallback) == "function")
+                {
                     successCallback(JSON.parse(files));
                 }
             }, errorCallback, "Filebrowser", "showMultiFolderpicker", [{
@@ -57,26 +69,32 @@ module.exports = {
             }]);
         }
     },
-    mixedPicker: function(config) {
-        var successCallback = config.success || function(){};
-        var errorCallback = config.error || function(){};
+    mixedPicker: function (config)
+    {
+        var successCallback = config.success || function () { };
+        var errorCallback = config.error || function () { };
         var startupPath = config.startupPath || "default";
 
-        cordova.exec(function(files){
-            if(typeof(successCallback) == "function"){
+        cordova.exec(function (files)
+        {
+            if (typeof (successCallback) == "function")
+            {
                 successCallback(JSON.parse(files));
             }
         }, errorCallback, "Filebrowser", "showMixedPicker", [{
             "start_directory": startupPath
         }]);
     },
-    createFileDialog: function(config){
-        var successCallback = config.success || function(){};
-        var errorCallback = config.error || function(){};
+    createFileDialog: function (config)
+    {
+        var successCallback = config.success || function () { };
+        var errorCallback = config.error || function () { };
         var startupPath = config.startupPath || "default";
 
-        cordova.exec(function(files){
-            if(typeof(successCallback) == "function"){
+        cordova.exec(function (files)
+        {
+            if (typeof (successCallback) == "function")
+            {
                 successCallback(JSON.parse(files));
             }
         }, errorCallback, "Filebrowser", "showCreatefile", [{
